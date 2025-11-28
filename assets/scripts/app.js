208622a6e1ced894040ded8525148a732acc6184
brayanjk13 <brayanjk13@gmail.com>
2025-11-28 11:08:08 -0500

    Se añadio la seccion de Reseñas con boton interactivo en JavaScript

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+// LÓGICA DEL ACORDEÓN PARA PREGUNTAS FRECUENTES
+const preguntas = document.querySelectorAll('.faq-question');
+
+preguntas.forEach(pregunta => {
+    pregunta.addEventListener('click', () => {
+        const item = pregunta.parentElement;
+    
+        // Alternar la clase 'active' para abrir/cerrar
+        item.classList.toggle('active');
+    });
+});
