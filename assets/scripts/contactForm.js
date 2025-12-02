d9f98b1b82ff37dd7296295ae0d020f418682804
Sebastian Tavara <sebastian040606@gmail.com>
2025-12-01 22:23:49 -0500

    Implementacion de la base de datos con Firebase para el formulario de contactanos

--- assets/scripts/contactForm.js
+++ assets/scripts/contactForm.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-librarie
 // Your web app's Firebase configuration
@@ -12,12 +14,14 @@ const firebaseConfig = {
   storageBucket: "blockvoluntariado.firebasestorage.app",
   messagingSenderId: "780129250896",
   appId: "1:780129250896:web:9c61c03b5c98667ceceb55",
-  measurementId: "G-PWSKFPJGDF"
+  measurementId: "G-PWSKFPJGDF",
+
+  databaseURL: "https://blockvoluntariado-default-rtdb.firebaseio.com/"
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
+const db= getDatabase(app);
 
 // para guardar el formulario
 const form = document.getElementById("contactForm");
@@ -28,7 +32,7 @@ form.addEventListener("submit", (e) => {
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
 
-    const correo = document.getElementById("correo").value;
+    const email = document.getElementById("email").value;
     const telefono = document.getElementById("telefono").value;
     const asunto = document.getElementById("asunto").value;
     const descripcion = document.getElementById("descripcion").value;
@@ -39,7 +43,7 @@ form.addEventListener("submit", (e) => {
     set(nuevo, {
         nombre,
         apellido,
-        correo,
+        email,
         telefono,
         asunto,
         descripcion,
